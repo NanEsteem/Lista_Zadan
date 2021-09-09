68a747fd0a3b45d07145197e7dcdb93526eef9da
NanEsteem <esteem1178@gmail.com>
2021-09-09 11:10:40 +0200

    Add new files

---
+++ components/Task.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props) => {
+
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+              <Text style={styles.itemText}>{props.text}</Text> 
+            </View>
+            <Text style={styles.x} onPress={() => props.completeTask(props.index)}>X</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        width: '90%'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText: {
+        width: '90%',
+    },
+    x: {
+        padding: 5,
+        width: '10%'        
+   },
+});
+
+export default Task;
